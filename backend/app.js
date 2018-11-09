1cc7dc01d19740c41fc7263018c9e2b54dd43793
moingeek <gadmoin@gmail.com>
2018-11-09 17:26:19 +0530

    Sign up Done

--- backend/app.js
+++ backend/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require("mongoose");
 const postsRoutes = require('./routes/posts');
+const userRoutes = require('./routes/user');
 const path = require('path');
 
 
@@ -28,6 +29,7 @@ app.use((req,res,next)=>{
 });
 
 app.use("/api/posts",postsRoutes);
+app.use("/api/user",userRoutes);
 
 
 module.exports = app;
